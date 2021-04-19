0e59b764c051271a435237ee0fb2ea58f384e1a1
Warrenww <warrenpig@gmail.com>
2021-04-19 10:23:17 +0800

    add selector to dashboard

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -3,55 +3,107 @@ import {
   Row,
   Col,
   Typography,
-  Checkbox,
   Table,
+  Select,
+  Form,
 } from 'antd';
 import {
   DashBoardProps,
 } from './types';
 import API from '../API';
-import { CheckboxCol } from './styles';
 
 const { Title } = Typography;
+const { Option } = Select;
+const { Item } = Form;
 
 const DashBoard = ({ filename }: DashBoardProps) => {
-  const [data, setData] = useState<any[]>([]);
+  const [datas, setDatas] = useState<any[]>([]);
+  const [selectedPlayer, setSelectedPlayer] = useState<string[]>([]);
+  const [selectedEvent, setSelectedEvent] = useState<string[]>([]);
 
   useEffect(() => {
-    API.get('/read?filename='+filename).then((res) => setData(res.data));
+    API.get('/read?filename='+filename).then((res) => setDatas(res.data));
   }, [filename]);
 
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    console.log(datas);
+  }, [datas]);
 
-  const worlds = useMemo(() => Array.from(new Set(data.map(d => d?.worldUUID ?? ''))), [data]);
-  const players = useMemo(() => Array.from(new Set(data.map(d => d?.player ?? ''))), [data]);
+  const worlds = useMemo(() => Array.from(new Set(datas.map(d => d?.worldUUID ?? ''))), [datas]);
+  const players = useMemo(() => Array.from(new Set(datas.map(d => d?.player ?? ''))), [datas]);
+  const events = useMemo(() => Array.from(new Set(datas.map(d => d?.event))), [datas]);
+  const displayData = useMemo(() => datas
+    .filter((d) => selectedPlayer.length ? selectedPlayer.includes(d.player ?? '') : d)
+    .filter((d) => selectedEvent.length ? selectedEvent.includes(d.event ?? '') : d)
+  , [datas, selectedPlayer]);
+
+  console.log(displayData);
+
+  const columns = [
+    {
+      title: 'Time',
+      dataIndex: 'time',
+      key: 'time',
+    },
+    {
+      title: 'Event',
+      dataIndex: 'event',
+      key: 'event',
+    },
+    {
+      title: 'Player',
+      dataIndex: 'player',
+      key: 'player',
+    },
+  ];
 
   return (
     <Row>
       <Col xs={24}>
         <Title>{filename}</Title>
       </Col>
-      <CheckboxCol xs={12}>
-        <Title level={2}> Worlds </Title>
-        <Checkbox.Group
-          options={worlds.filter(x => x!== '')}
-          defaultValue={['Apple']}
-          onChange={(values) => console.log(values)}
-        />
-      </CheckboxCol>
-      <CheckboxCol xs={12}>
-        <Title level={2}> Players </Title>
-        <Checkbox.Group
-          options={players.filter(x => x!== '')}
-          defaultValue={['Apple']}
-          onChange={(values) => console.log(values)}
-        />
-      </CheckboxCol>
+      <Col xs={24}>
+        <Form>
+          <Item label="World">
+            <Select mode="multiple" >
+              {
+                worlds.map((world: string, index: number) => (
+                  <Option key={`world-${index}`} value={world}>
+                    {world}
+                  </Option>
+                ))
+              }
+            </Select>
+          </Item>
+          <Item label="Player">
+            <Select mode="multiple" onChange={(values: string[]) => setSelectedPlayer(values)}>
+              {
+                players.map((player: string, index: number) => (
+                  <Option key={`player-${index}`} value={player}>
+                    {player}
+                  </Option>
+                ))
+              }
+            </Select>
+          </Item>
+          <Item label="Event">
+            <Select mode="multiple" onChange={(values: string[]) => setSelectedEvent(values)}>
+              {
+                events.map((event: string, index: number) => (
+                  <Option key={`event-${index}`} value={event}>
+                    {event}
+                  </Option>
+                ))
+              }
+            </Select>
+          </Item>
+        </Form>
+
+      </Col>
       <Col xs={24}>
         <Table
-            
+          columns={columns}
+          dataSource={displayData}
         />
       </Col>
     </Row>
